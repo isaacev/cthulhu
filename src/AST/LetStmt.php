cdb9745d002583775bc5768cce107a70e95840d3
isaacev <isaacevavold@gmail.com>
2019-08-21 22:37:14 -0500

    fix hint typo

--- src/AST/LetStmt.php
+++ src/AST/LetStmt.php
@@ -8,7 +8,7 @@ class LetStmt extends Stmt {
   public $name;
   public $expr;
 
-  function __construct(Span $span, string $name, expr $expr) {
+  function __construct(Span $span, string $name, Expr $expr) {
     parent::__construct($span);
     $this->name = $name;
     $this->expr = $expr;
