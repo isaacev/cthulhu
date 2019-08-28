3e1e7750224920e3218940930a860ebf85e71ca2
isaacev <isaacevavold@gmail.com>
2019-08-27 23:01:11 -0500

    add compile script

---
+++ compile.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+if ($argc < 2) {
+  echo 'USAGE: ./compile <filename>' . PHP_EOL;
+  exit(1);
+}
+
+$intput_filename = $argv[1];
+$basename = basename($intput_filename);
+$txt = @file_get_contents($intput_filename);
+if ($txt === false) {
+  echo "unable to get contents of '$intput_filename'" . PHP_EOL;
+  exit(1);
+}
+
+$ref = new \Cthulhu\Codegen\PHP\Reference([ $basename, 'main' ]);
+$ast = \Cthulhu\Parser\Parser2::from_string($txt)->file();
+$mod = \Cthulhu\Analysis\Analyzer2::file($basename, $ast);
+$php = \Cthulhu\Codegen\Codegen2::generate($mod, $ref);
+$str = $php->build()->write(new \Cthulhu\Codegen\StringWriter());
+
+$output_filename = $basename . '.php';
+$output_file = fopen($output_filename, 'w') or die('unable to open file');
+fwrite($output_file, $str);
+fclose($output_file);
+
+echo "compiled $intput_filename to $output_filename" . PHP_EOL;
