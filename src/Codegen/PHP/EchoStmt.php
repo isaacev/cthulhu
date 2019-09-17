ccb7d3e5b69230e1b1c02d7fbc8323ba04f083d6
isaacev <isaacevavold@gmail.com>
2019-09-17 14:49:38 -0500

    add php echo statement

---
+++ src/Codegen/PHP/EchoStmt.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Cthulhu\Codegen\PHP;
+
+use Cthulhu\Codegen\Builder;
+
+class EchoStmt extends Stmt {
+  public $expr;
+
+  function __construct(Expr $expr) {
+    $this->expr = $expr;
+  }
+
+  public function build(): Builder {
+    return (new Builder)
+      ->keyword('echo')
+      ->space()
+      ->expr($this->expr)
+      ->semicolon();
+  }
+}
