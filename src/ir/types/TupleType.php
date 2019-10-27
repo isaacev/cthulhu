5135f54328218bc31702b3e9f896d0a67cb4e8df
isaacev <isaacevavold@gmail.com>
2019-10-27 13:55:44 -0500

    add tuple type

---
+++ src/ir/types/TupleType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Cthulhu\ir\types;
+
+class TupleType extends Type {
+  public $members;
+
+  function __construct(array $members) {
+    assert(count($members) > 1);
+    $this->members = $members;
+  }
+
+  function size(): int {
+    return count($this->members);
+  }
+
+  function accepts(Type $other): bool {
+    if ($other instanceof self) {
+      if ($this->size() === $other->size()) {
+        for ($i = 0; $i < $this->size(); $i++) {
+          if ($this->members[$i]->accepts($other->members[$i]) === false) {
+            return false;
+          }
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function unify(Type $other): ?Type {
+    if ($other instanceof self) {
+      if ($this->size() === $other->size()) {
+        $new_members = [];
+        for ($i = 0; $i < $this->size(); $i++) {
+          if ($unified = $this->members[$i]->unify($other->members[$i])) {
+            $new_members[] = $unified;
+          } else {
+            return null;
+          }
+        }
+        return new self($new_members);
+      }
+    }
+    return null;
+  }
+
+  function replace_generics(array $replacements): Type {
+    $new_members = [];
+    foreach ($this->members as $member) {
+      $new_members[] = $member->replace_generics($replacements);
+    }
+    return new self($new_members);
+  }
+
+  function __toString(): string {
+    return '(' . implode(',', $this->members) . ')';
+  }
+}
