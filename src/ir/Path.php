100d3dc469644c5ab41d6efd4b346b254eafc0da
isaacev <isaacevavold@gmail.com>
2019-11-13 13:26:41 -0600

    remove unused method

--- src/ir/Path.php
+++ src/ir/Path.php
@@ -14,15 +14,4 @@ class Path {
   function extend(nodes\Node $node): self {
     return new self($this, $node);
   }
-
-  function nearest_module(): ?nodes\ModItem {
-    $ancestor = $this->parent;
-    while ($ancestor !== null) {
-      if ($ancestor->node instanceof nodes\ModItem) {
-        return $ancestor->node;
-      }
-      $ancestor = $ancestor->parent;
-    }
-    return null;
-  }
 }
