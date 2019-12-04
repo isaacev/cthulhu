4d319a7059c684a369ce830cb7064617fa5cf236
isaacev <isaacevavold@gmail.com>
2019-12-04 15:37:10 -0600

    improve tree shaking algorithm

--- src/php/passes/TreeShaking.php
+++ src/php/passes/TreeShaking.php
@@ -7,39 +7,72 @@ use Cthulhu\php\visitor;
 
 class TreeShaking {
   public static function apply(php\nodes\Program $prog): php\nodes\Program {
-    $declared = [];
-    $used     = [];
+    $stack = [];
+    $graph = [];
+    $reach = [];
+
+    $add_edge = function (int $from, int $to) use (&$graph) {
+      if (array_key_exists($from, $graph) === false) {
+        $graph[$from] = [ $to ];
+      } else if (in_array($to, $graph[$from]) === false) {
+        $graph[$from][] = $to;
+      }
+    };
 
     visitor\Visitor::walk($prog, [
-      'ReferenceExpr' => function (visitor\Path $path) use (&$used) {
-        assert($path->node instanceof php\nodes\ReferenceExpr);
-        $symbol_id = $path->node->reference->symbol->get_id();
-        if (!in_array($symbol_id, $used)) {
-          $used[] = $symbol_id;
+      'preorder(FuncStmt)' => function (visitor\Path $path) use (&$stack) {
+        assert($path->node instanceof php\nodes\FuncStmt);
+        array_push($stack, $path->node->head->name->symbol->get_id());
+      },
+      'postorder(FuncStmt)' => function () use (&$stack) {
+        array_pop($stack);
+      },
+      'ClassStmt' => function (visitor\Path $path) use (&$add_edge) {
+        assert($path->node instanceof php\nodes\ClassStmt);
+        $class_id = $path->node->name->symbol->get_id();
+        if ($path->node->parent_class !== null) {
+          $parent_id = $path->node->parent_class->symbol->get_id();
+          $add_edge($class_id, $parent_id);
         }
       },
-      'FuncStmt' => function (visitor\Path $path) use (&$declared) {
-        assert($path->node instanceof php\nodes\FuncStmt);
-        $symbol_id = $path->node->head->name->symbol->get_id();
-        if (!in_array($symbol_id, $declared)) {
-          $declared[] = $symbol_id;
+      'ReferenceExpr' => function (visitor\Path $path) use (&$stack, &$add_edge, &$reach) {
+        assert($path->node instanceof php\nodes\ReferenceExpr);
+        $ref_id       = $path->node->reference->symbol->get_id();
+        $func_stmt_id = end($stack);
+        if ($func_stmt_id === false) {
+          // If the ReferenceExpr is not inside of a function that it will
+          // always be run by the program so mark it as already reachable.
+          $reach[$ref_id] = false;
+        } else {
+          $add_edge($func_stmt_id, $ref_id);
         }
       },
     ]);
 
-    // Remove any symbols ids that are declared but not used
-    $to_remove = [];
-    foreach ($declared as $id) {
-      if (!in_array($id, $used)) {
-        $to_remove[] = $id;
+    $queue = array_keys($reach);
+    while ($next_id = array_shift($queue)) {
+      if (isset($reach[$next_id]) && $reach[$next_id] === true) {
+        continue;
+      }
+
+      $reach[$next_id] = true;
+      if (array_key_exists($next_id, $graph)) {
+        array_push($queue, ...$graph[$next_id]);
       }
     }
 
     $new_prog = visitor\Visitor::edit($prog, [
-      'FuncStmt' => function (visitor\Path $path) use (&$to_remove) {
+      'FuncStmt' => function (visitor\Path $path) use (&$reach) {
         assert($path->node instanceof php\nodes\FuncStmt);
         $symbol_id = $path->node->head->name->symbol->get_id();
-        if (in_array($symbol_id, $to_remove)) {
+        if (array_key_exists($symbol_id, $reach) === false) {
+          $path->remove();
+        }
+      },
+      'ClassStmt' => function (visitor\Path $path) use (&$reach) {
+        assert($path->node instanceof php\nodes\ClassStmt);
+        $symbol_id = $path->node->name->symbol->get_id();
+        if (array_key_exists($symbol_id, $reach) === false) {
           $path->remove();
         }
       },
