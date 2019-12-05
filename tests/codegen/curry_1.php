fd8e118d4624b6f2d72d552ed4a2a094e9776347
isaacev <isaacevavold@gmail.com>
2019-12-05 16:06:28 -0600

    print result from curry test

--- tests/codegen/curry_1.php
+++ tests/codegen/curry_1.php
@@ -1,5 +1,33 @@
 <?php
 
+namespace Kernel\Builtins {
+  // #[inline]
+  // #[construct]
+  function _print($a) {
+    print($a);
+  }
+
+  // #[inline]
+  // #[construct]
+  function cast_int_to_string($a) {
+    return (string)$a;
+  }
+}
+
+namespace Io {
+  // #[inline]
+  function println($str) {
+    \Kernel\Builtins\_print($str . "\n");
+  }
+}
+
+namespace Fmt {
+  // #[inline]
+  function int($i) {
+    return \Kernel\Builtins\cast_int_to_string($i);
+  }
+}
+
 namespace curry_1 {
   function foo($a, $b, $c) {
     return ($a + $b) * $c;
@@ -8,7 +36,7 @@ namespace curry_1 {
   // #[entry]
   function main() {
     $x = '\curry_1\foo';
-    $x(1, 2, 3) == 9;
+    \Io\println(\Fmt\int($x(1, 2, 3)));
   }
 }
 
