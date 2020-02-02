29cd79a3f763f7180f7cbe9b3a3b5fd02109b2bf
isaacev <isaacevavold@gmail.com>
2020-02-02 09:40:43 -0600

    when combining zero and unknown arities, defer to unknown

--- src/ir/arity/ZeroArity.php
+++ src/ir/arity/ZeroArity.php
@@ -4,6 +4,9 @@ namespace Cthulhu\ir\arity;
 
 class ZeroArity extends Arity {
   public function combine(Arity $other): Arity {
+    if ($other instanceof UnknownArity) {
+      return $other;
+    }
     assert($other instanceof ZeroArity);
     return $this;
   }
