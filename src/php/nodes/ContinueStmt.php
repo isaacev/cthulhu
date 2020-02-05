0d75b75fc0a49e55c159ac85faf047c434749282
isaacev <isaacevavold@gmail.com>
2020-02-04 21:27:49 -0600

    add continue statement

---
+++ src/php/nodes/ContinueStmt.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cthulhu\php\nodes;
+
+use Cthulhu\lib\trees\EditableSuccessor;
+use Cthulhu\php\Builder;
+
+class ContinueStmt extends Stmt {
+  use traits\Atomic;
+
+  public function from_successor(?EditableSuccessor $successor): ContinueStmt {
+    assert($successor === null || $successor instanceof Stmt);
+    return new ContinueStmt($successor);
+  }
+
+  public function build(): Builder {
+    return (new Builder)
+      ->newline_then_indent()
+      ->keyword('continue')
+      ->semicolon()
+      ->then($this->next ?? (new Builder));
+  }
+}
